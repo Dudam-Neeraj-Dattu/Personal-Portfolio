3f221cc7b5bd813180b09244b3f89ed28df8ed2c
Neerajdattu Dudam <dudamdattu@gmail.com>
2025-05-27 19:43:14 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {  
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+         <h1 className='text-4xl font-bold text-gray-900'>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
