1ea79811b2ebfcc2f6a60c0c1bcfe6d367663977
Neerajdattu Dudam <dudamdattu@gmail.com>
2025-05-31 15:41:44 -0500

    modified indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
-function App() {  
+const App = () => {
+   return (
+      <div>
 
-  return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-         <h1 className='text-4xl font-bold text-gray-900'>Hello world</h1>
       </div>
-    </>
-  )
+   )
 }
 
 export default App
