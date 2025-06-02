5996dbc30127d111254f8abbf5af5ff5194e6128
Neerajdattu Dudam <dudamdattu@gmail.com>
2025-06-02 16:03:41 -0500

    started herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavbarMain from './components/navbar/NavbarMain'
+import HeroMain from './components/heroSection/HeroMain'
 
 const App = () => {
    return (
       <main className='font-body'>
          <NavbarMain/>
+         <HeroMain/>
       </main>
    )
 }
